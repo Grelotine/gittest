6f809529c9d3aabb5a36d0b1a0fdf3e687645b46
Grelotine <none@none>
2018-08-01 14:54:39 +0200

    Pom init

---
+++ src/main/org/squashtest/tf/gittest/main/Application.java
@@ -0,0 +1,11 @@
+package org.squashtest.tf.gittest.main;
+
+/**
+ * Created by jlor on 01/08/2018.
+ */
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Git.");
+    }
+}
